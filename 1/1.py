299fffb91f31c8b577abaf92030d1f4a2e1c2884
Pablo Fontanilla <pfontanilla@medida.com>
2023-12-07 12:50:44 +0100

    Day 1 - Problem 2

--- 1/1.py
+++ 1/1.py
@@ -1,14 +1,38 @@
 import re
 
 
+def letters_to_number(number: str) -> str:
+    match number:
+        case "one":
+            return '1'
+        case "two":
+            return '2'
+        case "three":
+            return '3'
+        case "four":
+            return '4'
+        case "five":
+            return '5'
+        case "six":
+            return '6'
+        case "seven":
+            return '7'
+        case "eight":
+            return '8'
+        case "nine":
+            return '9'
+        case _:
+            return number
+
+
 def get_first_digit(line: str) -> str:
 
-    return re.search('(\d)(.*)', line).group(1)
+    return letters_to_number(re.search('(\d|one|two|three|four|five|six|seven|eight|nine)(.*)', line).group(1))
 
 
 def get_last_digit(line: str) -> str:
 
-    return re.search('(.*)(\d)', line).group(2)
+    return letters_to_number(re.search('(.*)(\d|one|two|three|four|five|six|seven|eight|nine)', line).group(2))
 
 
 total: int = 0
